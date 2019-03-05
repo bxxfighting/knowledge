2f14ccde96efc1fe489bcb6a5df3134cca8be0f2
bxx <buxingxing@chengantech.com>
2019-03-05 18:06:54 +0800

    go啥go

---
+++ algorithm/lowest/merge.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 11, 33, 55, 66}
+    var b = [...]int{1, 4, 5, 8, 12, 42, 45, 51, 63, 78, 88}
+    fmt.Println(a)
+    fmt.Println(b)
+    var result []int
+
+    var a_len int = len(a)
+    var b_len int = len(b)
+    var lengths = a_len + b_len
+    var a_site int = 0
+    var b_site int = 0
+
+    for i := 0; i < lengths; i ++ {
+        if a_site >= a_len {
+            for j := b_site; j < b_len; j ++ {
+                result = append(result, b[j])
+            }
+            break
+        }
+        if b_site >= b_len {
+            for j := a_site; j < a_len; j ++ {
+                result = append(result, a[j])
+            }
+            break
+        }
+        if a[a_site] <= b[b_site] {
+            result = append(result, a[a_site])
+            a_site += 1
+        } else {
+            result = append(result, b[b_site])
+            b_site += 1
+        }
+
+    }
+    fmt.Println(result)
+}
