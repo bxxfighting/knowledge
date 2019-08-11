5a92f68c69237dfa452d1915032d6c85b8d4f8ff
卜星星 <boxingxing@limikeji.com>
2019-08-11 18:34:14 +0800

    增加left/right说明

--- algorithm/quicksort.py
+++ algorithm/quicksort.py
@@ -9,10 +9,15 @@ def quick_sort(numbers, left, right):
             while left < right and numbers[right] > key:
                 right -= 1
             numbers[left] = numbers[right]
+            # 这里这个left += 1可以不要，如果写了的话
+            # 在2号位标识之后，都应该使用right，而不能用left
+            # 如果把这个left += 1去掉的话，2号位标识之后的地方
+            # 可以用left也可以用right
             left += 1
             while left < right and numbers[left] < key:
                 left += 1
             numbers[right] = numbers[left]
+        # 2号位
         numbers[right] = key
         quick_sort(numbers, start, right - 1)
         quick_sort(numbers, right + 1, end)
